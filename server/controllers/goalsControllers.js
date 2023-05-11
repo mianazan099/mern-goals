72eafc691348c395172a19fa519915cb391a8d5c
mian azan <mianazan024@gmail.com>
2023-05-11 22:41:40 +0500

    protect goal routes

--- server/controllers/goalsControllers.js
+++ server/controllers/goalsControllers.js
@@ -6,7 +6,7 @@ const Goal = require("../models/goalModel");
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
@@ -18,7 +18,7 @@ const setGoals = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text");
   }
-  const goal = await Goal.create({ text: req.body.text });
+  const goal = await Goal.create({ user: req.user.id, text: req.body.text });
   res.status(200).json(goal);
 });
 
@@ -26,14 +26,18 @@ const setGoals = asyncHandler(async (req, res) => {
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoals = asyncHandler(async (req, res) => {
-  const goal = await Goal.findById(req.params.id);
-  if (!goal) {
+  const goal = await Goal.find({ user: req.user.id, _id: req.params.id });
+  if (goal.length === 0) {
     res.status(400);
     throw new Error("the goal was not found");
   }
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+  const updatedGoal = await Goal.findOneAndUpdate(
+    { user: req.user.id, _id: req.params.id },
+    req.body,
+    {
+      new: true,
+    }
+  );
   res.status(200).json(updatedGoal);
 });
 
@@ -41,12 +45,12 @@ const updateGoals = asyncHandler(async (req, res) => {
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoals = asyncHandler(async (req, res) => {
-  const goal = await Goal.findById(req.params.id);
-  if (!goal) {
+  const goal = await Goal.find({ user: req.user.id, _id: req.params.id });
+  if (goal.length === 0) {
     res.status(400);
     throw new Error("the goal was not found");
   }
-  await Goal.findByIdAndRemove(req.params.id);
+  await Goal.findOneAndRemove({ user: req.user.id, _id: req.params.id });
   res.status(200).json({ id: req.params.id });
 });
 
